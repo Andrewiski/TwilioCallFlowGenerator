82d0a55544a256fef19c313376ecba1ea6b181c8
Andrew DeVries <adevries@digitalexample.com>
2022-05-27 13:05:49 -0400

    Add upload tp test

--- test/test.js
+++ test/test.js
@@ -15,6 +15,68 @@ app.disable('x-powered-by');
 app.use(favicon(path.join(parentPath, '/assets/favicon.ico')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+let rootUrlServerless = "https://serverless-upload.twilio.com/v1";
+
+var createFunctionVersion = function(options){ 
+    var deferred = $.Deferred();
+  
+    try{         
+      var url = rootUrlServerless + "/Services/" + options.ServiceSid + "/Functions/" + options.FunctionSid + "/Versions" ;
+      var formData = new FormData();
+      getRepoFileContents({url:options.contentUrl}).then(
+        function(repoFileData){
+          //var content = data.fileContent; // the body of the new file...
+          var blob = base64toBlob(repoFileData.content,"application/javascript") //new Blob([content], { type: "text/javascript"});
+          formData.append("Path", options.path);
+          formData.append("Visibility", "public")
+          formData.append(options.fileName, blob);
+          var ajaxOptions = {
+            url: url, 
+            headers: {"Authorization": "Basic " + btoa(options.accountSid + ":" + options.authToken)},
+            type: "POST",
+            contentType: 'multipart/form-data',
+            processData: false,
+            // xhrFields: {
+            //   withCredentials: true
+            // },
+            crossDomain: false,
+            dataType: 'json',
+            data: formData
+            //crossDomain: true
+          }
+          $.ajax(ajaxOptions).then(
+            function(functionVersionData){
+              deferred.resolve(functionVersionData);
+            },
+            function( jqXHR, textStatus, errorThrown){
+                deferred.reject(errorThrown);
+              }
+          )
+        },
+        function(ex){
+          deferred.reject(ex);
+        }
+      )
+      
+    }catch(ex){
+      deferred.reject(ex);
+    }
+    return deferred.promise();
+}
+
+app.use("/createFunctionVersion", function(req,res, next){
+    createFunctionVersion(req.data).then(
+        function(retval){
+            res.status(200).send(retval);
+        },
+        function(ex){
+            res.status(500).send(ex);
+            
+        }
+    )
+    
+})
 //app.use(cookieParser());
 var http_srv=null;
 try {
